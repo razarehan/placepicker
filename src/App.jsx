89da1676805c10977346fe317b1d21114b857604
rzzza_rehan <rhn.raza121@gmail.com>
2024-09-10 23:24:44 +0530

    Close modal by ESC bug fixes

--- src/App.jsx
+++ src/App.jsx
@@ -69,7 +69,7 @@ function App() {
   }, [])
   return (
     <>
-      <Modal open={modalIsOpen}>
+      <Modal open={modalIsOpen} onClose={handleStopRemovePlace}>
         <DeleteConfirmation
           onCancel={handleStopRemovePlace}
           onConfirm={handleRemovePlace}
